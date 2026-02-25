ab4df4de5853fc33668044dd49d250382a5c5300
svein <sveinsmurf@proton.me>
2026-02-25 13:06:58 +0100

    Clean up ToggleDeathScreen.cs

--- Assets/Scripts/ToggleDeathScreen.cs
+++ Assets/Scripts/ToggleDeathScreen.cs
@@ -3,18 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class ToggleDeathScreen : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.O))
         {
-            Debug.Log("space er blitt trykket");
             SceneManager.LoadScene("DeathScreen");
         }
     }
