21d6c9ba00c07124790eb843cb7805379f54eb6b
C2Hunter.org <supp@c2hunter.org>
2026-03-04 10:21:30 +0100

    Added attack slashing, prefab system, and attack effect lifetime & cooldown.

--- Assets/Scripts/AimingFireSlash.cs
+++ Assets/Scripts/AimingFireSlash.cs
@@ -1,39 +1,49 @@
-using System;
 using UnityEngine;
 
 public class AimingFireSlash : MonoBehaviour
 {
-    DateTime lastSlash = DateTime.MinValue;
+    float lastSlashTime;
     bool attackSlash = false;
+    public GameObject airSlashPrefab;
+    public float slashSpeed = 10f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        lastSlashTime = Time.time - 1f; // Player can slash immediately at the start of the game
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            attackSlash = true;
-        }
-        else
-        {
-            attackSlash = false;
-        }
-    }
+        // Hold left mouse button to keep attacking
+        attackSlash = Input.GetKey(KeyCode.Mouse0);
 
-    private void FixedUpdate()
-    {
-        if (attackSlash && (DateTime.Now - lastSlash).Seconds < 2)
+        if (attackSlash && Time.time - lastSlashTime >= 1f)
         {
-            lastSlash = DateTime.Now;
-            Debug.Log($"Slash {DateTime.Now.ToString("T")}");
-        }
-        else
-        {
+            lastSlashTime = Time.time;
+            // Spawn a copy of the AirSlashSprite at the current position/rotation and launch it
+            if (airSlashPrefab != null)
+            {
+                Quaternion prefabRotation = airSlashPrefab.transform.rotation;
+                GameObject slash = Instantiate(airSlashPrefab, transform.position, prefabRotation);
+                slash.transform.localScale = airSlashPrefab.transform.lossyScale;
 
+                Vector2 direction = transform.up.normalized;
+
+                AirSlash mover = slash.GetComponent<AirSlash>();
+                if (mover != null)
+                {
+                    mover.Launch(direction, slashSpeed);
+                }
+                else
+                {
+                    Debug.LogWarning("AirSlash prefab has no AirSlash component");
+                }
+            }
+            else
+            {
+                Debug.Log($"Slash {Time.time:F2}");
+            }
         }
     }
 }
