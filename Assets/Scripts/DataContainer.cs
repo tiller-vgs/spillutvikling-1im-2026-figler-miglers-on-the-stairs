56022823b91301f3aadfb9dd666bd63c05a8407e
C2Hunter.org <supp@c2hunter.org>
2026-03-10 13:33:35 +0100

    Added DataContainer and update healthservice.

---
+++ Assets/Scripts/DataContainer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Assets.Scripts
+{
+	public class DataContainer
+	{
+		static Dictionary<string, object> storage = new Dictionary<string, object>();
+
+		public static string GetObjectAsString(string key)
+		{
+			return (string)storage[key];
+		}
+
+		public static int GetObjectAsInt(string key)
+		{
+			return (int)storage[key];
+		}
+
+		public static float GetObjectAsFloat(string key)
+		{
+			return (float)storage[key]; 
+		}
+
+		public static bool GetObjectAsBool(string key)
+		{
+			return (bool)storage[key];
+        }
+
+		public static object GetObject(string key)
+		{
+			return storage[key]; 
+		}
+
+        public static void SetObject(string key, object value)
+		{
+			storage[key] = value; 
+		}
+
+        /*
+         * SetObject("CurrentLevel", "Level1")
+		 * GetObjectAsString("CurrentLevel") => "Level1"
+		 */
+    }
+}
